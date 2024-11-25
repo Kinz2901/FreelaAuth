829272677346553815a6ac2f1390d825abb87a40
Guilherme Lins <guilhermelins978@gmail.com>
2024-11-25 13:37:26 -0300

    Criei a funcão de mostrar e ocultar a senha

--- script.js
+++ script.js
@@ -10,11 +10,36 @@ const signInForm = document.getElementById('signIn');
 // Seleciona o formulário de cadastro
 const signUpForm = document.getElementById('signup');
 
-// Seleciona o icone de cadeado da senha
+const rPassword = document.querySelector("#rPassword")
 const lockPassword = document.querySelector("#lockPassword")
+const unlockPassword = document.querySelector("#unlockPassword")
 
+const confirmPassword = document.querySelector("#confirmPassword")
+const lockConfirmPassword = document.querySelector("#lockConfirmPassword")
+const unlocKConfirmPassword = document.querySelector("#unlockConfirmPassword")
+
+// Mostrar password e password confirm
 lockPassword.addEventListener("click", function() {
-  console.log("Foi")
+  lockPassword.style.display = "none";
+  unlockPassword.style.display = "flex"
+  rPassword.type = "string"
+})
+lockConfirmPassword.addEventListener("click", function() {
+  lockConfirmPassword.style.display = "none";
+  unlocKConfirmPassword.style.display = "flex"
+  confirmPassword.type = "string"
+})
+
+// Ocultar password e password confirm
+unlockPassword.addEventListener("click", function() {
+  unlockPassword.style.display = "none";
+  lockPassword.style.display = "flex"
+  rPassword.type = "password"
+})
+unlocKConfirmPassword.addEventListener("click", function() {
+  unlocKConfirmPassword.style.display = "none";
+  lockConfirmPassword.style.display = "flex"
+  confirmPassword.type = "password"
 })
 
 // Adiciona um ouvinte de eventos ao botão de cadastro
